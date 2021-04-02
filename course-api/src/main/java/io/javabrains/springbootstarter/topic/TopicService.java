436dfe4b13e2f30a70d02b435b5c7f657320acee
junwchoi85 <42979340+junwchoi85@users.noreply.github.com>
2021-04-03 06:00:45 +0900

    adding new service

--- course-api/src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
+++ course-api/src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
@@ -1,5 +1,6 @@
 package io.javabrains.springbootstarter.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,13 +9,21 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 
-	private List<Topic> topics = Arrays.asList(
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(
 			new Topic("spring", "spring framework", "spring framework description"),
 			new Topic("java", "core java", "core java description"),
 			new Topic("javascript", "javascript", "javascript description")
-			);
+			));
 	
 	public List<Topic> getAllTopics() {
 		return topics;
+	}
+	
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+	
+	public void addTopic(Topic topic) {
+		topics.add(topic);
 	}
 }
